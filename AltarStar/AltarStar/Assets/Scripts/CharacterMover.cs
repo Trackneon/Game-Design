dd14710adc6f7bad522068cac46817f7d220a738
Tyler Hales <10869186@my.uvu.edu>
2020-02-05 21:26:31 -0700

    2/5

---
+++ AltarStar/AltarStar/Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterMover : MonoBehaviour
+{
+    public float moveSpeed = 100f; 
+    private Vector3 position;
+    private CharacterController controller;
+    
+   
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        position.x = moveSpeed * Input.GetAxis("Horizontal");
+        position.z = moveSpeed * Input.GetAxis("Vertical");
+        controller.Move(position * Time.deltaTime);
+    }
+}

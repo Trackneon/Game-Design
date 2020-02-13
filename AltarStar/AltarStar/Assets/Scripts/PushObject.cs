477d338673127f1add9b20e68d67204778943ba6
Tyler Hales <10869186@my.uvu.edu>
2020-02-13 01:13:18 -0700

    2/13

---
+++ AltarStar/AltarStar/Assets/Scripts/PushObject.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PushObject : MonoBehaviour
+{
+    public float ObMass = 300;
+    public float PushAtMass = 100;
+    public float PushingTime;
+    public float ForceToPush;
+    Rigidbody rb;
+    public float vel;
+    Vector3 dir;
+
+    Vector3 lastPos;
+    float _PushingTime = 0;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        if (rb == null) return;
+        rb.mass = ObMass;
+    }
+
+    bool IsMoving()
+    {
+        if (rb.velocity.magnitude > 0.06f)
+        {
+            return true;
+        }
+        return false;
+
+    }
+
+    private void Update()
+    {
+        //F key to Push
+        vel = rb.velocity.magnitude;
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            rb.isKinematic = true;
+        }
+
+        if (rb.isKinematic == false)
+        {
+            _PushingTime += Time.deltaTime;
+            if (_PushingTime >= PushingTime)
+            {
+                _PushingTime = PushingTime;
+            }
+
+            rb.mass = Mathf.Lerp(ObMass, PushAtMass, _PushingTime / PushingTime);
+            rb.AddForce(dir * ForceToPush, ForceMode.Force);
+        }
+        else
+        {
+            rb.mass = ObMass;
+            _PushingTime = 0;
+
+        }
+    }
+        void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+
+            if (Input.GetKey(KeyCode.C))
+            {
+                rb.isKinematic = false;
+
+                dir = collision.contacts[0].point - transform.position;
+                // We then get the opposite (-Vector3) and normalize it
+                dir = -dir.normalized;
+
+            }
+        }
+
+    }
+
+}
---
+++ AltarStar/AltarStar/Assets/Scripts/TeleportObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportObject : MonoBehaviour
+{
+    public Transform hold;
+
+    void Update()
+    {
+        if (Input.GetKeyDown("q"))
+        {
+            GetComponent<BoxCollider>().enabled = false;
+            GetComponent<Rigidbody>().useGravity = false;
+            this.transform.position = hold.position;
+            this.transform.parent = GameObject.Find("PlayerHands").transform;
+        }
+
+        else
+        {
+            this.transform.parent = null;
+            GetComponent<Rigidbody>().useGravity = true;
+            GetComponent<BoxCollider>().enabled = true;
+        }
+    }
+
+
+
+}

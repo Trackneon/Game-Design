c79882074ba0e5f499078c4c3ca708b26b58cfaf
Tyler Hales <10869186@my.uvu.edu>
2020-02-18 22:45:09 -0700

    2/18

---
+++ AltarStar/AltarStar/Assets/Scripts/InstanceObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "Instancing Object")]
+public class InstanceObject : MonoBehaviour
+{
+    public GameObject prefab;
+
+    public void CreateInstance(GameObject instance)
+    {
+        Instantiate(instance);
+    }
+    
+    public void CreateInstanceAtLocation(Vector3Data location)
+    {
+        Instantiate(prefab, location.value, Quaternion.identity);
+    }
+}

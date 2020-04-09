91bc257a60a0b18fbd455a6ddfb193e10a004968
Tyler Hales <10869186@my.uvu.edu>
2020-04-09 14:02:04 -0600

    4/9

---
+++ AltarStar/AltarStar/Assets/Scripts/AreaTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaTrigger : MonoBehaviour
+{
+    public GameObject door;
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            door.SetActive(false);
+        }
+
+    }
+}
---
+++ AltarStar/AltarStar/Assets/Scripts/MiniBossDoor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniBossDoor : MonoBehaviour
+{
+    public GameObject door;
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            door.SetActive(true);
+        }
+
+    }
+}
